62b99d18bb9e06b126003c257546ac58b50b91dd
Fangliang Wang <Fangliang Wang>
2016-07-31 23:33:22 +0800

    chapter_06

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,9 +39,14 @@ class  NewVisitorTest(LiveServerTestCase):
         #她的爱好是使用假绳做诱饵钓鱼
         inputbox.send_keys("Buy peacock feathers")
 
-        #她按回车后，页面跟新了
-        #代办事项表格中显示了“1：Buy peacock feathers”
-        inputbox.send_keys(Keys.ENTER)
+        #她按回车后，被带到了一个新的URL
+        #这个页面的代办事项表格中显示了“1：Buy peacock feathers”
+
+        inputbox.send_keys(Keys.ENTER) 
+        edith_list_url = self.browser.current_url 
+        self.assertRegex(edith_list_url, '/lists/.+') 
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
+
 
         #页面中又显示了一个文本框，可以输入其他的代办事项
         #她输入了“Use peacock feathers to make a fly”
@@ -50,10 +55,41 @@ class  NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
     
         #页面再次跟新，她的清单中显示了这两个代办事项
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        
 
-        self.fail('Finish the test!')
+        #现在弗朗西斯的心用户访问了网站
+        ##我们使用一个新的浏览器会话
+        ##确保伊迪斯的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #弗朗西斯访问首页
+        #页面中看不到伊迪斯的清单
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        #弗朗西斯输入一个新待办事项，新建一个清单
+        #他不像伊迪斯那样兴趣盎然
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #弗朗西斯获得了他的唯一一个URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #这个页面还是么有伊迪斯的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from lists.models import Item
+from lists.models import List
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text = new_item_text)
-        return redirect('/')
-    items = Item.objects.all()
-    return render(request,'home.html',{'items':items})
+    return render(request,'home.html')
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_)
+    return redirect('/lists/%d/' %(list_.id,))
+
+def add_item(request,list_id):
+    list_ = List.objects.get(id = list_id)
+    Item.objects.create(text=request.POST['item_text'],list=list_)
+    return redirect('/lists/%d/' %(list_.id,))
+
+def view_list(request,list_id):
+    list_ = List.objects.get(id = list_id)
+    return render(request,'list.html',{'list':list_})
 
 
-    
