00d7c6e52d58ab36e65ae28969ab6070c78c48ef
Fangliang Wang <Fangliang Wang>
2016-08-03 20:05:54 +0800

    first steps of FT for layout+styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,6 +90,29 @@ class  NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("Buy peacock feathers", page_text)
         self.assertIn("Buy milk", page_text)
 
+    def test_layout_and_styling(self):
+        #伊迪斯访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #她看到输入框居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5        
+        )
+
+        #她新建一个菜单，看输入框是否居中
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5        
+        )        
+        
+
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
